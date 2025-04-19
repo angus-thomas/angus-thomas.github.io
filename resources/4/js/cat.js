81366c1fb5361f2ab0cac7914c897c19e7b2336b
angus-thomas <angusthomas12@gmail.com>
2025-04-19 22:15:28 +0200

    Cat's sleepy

--- resources/4/js/cat.js
+++ resources/4/js/cat.js
@@ -10,6 +10,7 @@ const WALK_ANIMATION_FRAMES = [
 	"-200px -100px",
 	"-100px -100px",
 ]
+const SLEEP_ANIMATION_FRAME = "0 -200px";
 
 class Cat {
 	
@@ -27,6 +28,9 @@ class Cat {
 		this.moving = false;
 		this.moveAnimationIndex = 0;
 		
+		this.asleep = true;
+		this.sleepTImerSet = false;
+		this.goToSleep();
 	}
 	
 	setPosition(x, y) {
@@ -36,11 +40,53 @@ class Cat {
 		this.elem.style.top = parseInt(this.y) - 25 + "px";
 	}
 	
-	move(x, y) {
+	goToSleep() {
+		this.preventSleep();
+		this.asleep = true;
+		this.elem.style.backgroundPosition = SLEEP_ANIMATION_FRAME;
+	}
+	
+	awake() {
+		this.asleep = false;
+		this.elem.style.backgroundPosition = WALK_ANIMATION_FRAMES[this.moveAnimationIndex];
+		this.queueSleep()
+	}
+	
+	preventSleep() {
+		if (this.sleepTImerSet) {
+			clearInterval(this.sleepInterval);
+		}
+	}
+	
+	queueSleep() {
+		this.preventSleep();
+		this.sleepTImerSet = true;
+		this.sleepInterval = setInterval(this.goToSleep.bind(this), 5000);
+	}
+	
+	clickCat() {
+		if (this.asleep) {
+			this.awake();
+		} else if (!this.moving) { // I should probably create a cat finite state machine
+			// Reset the sleep counter
+			this.queueSleep();
+		}
+	}
+	
+	clickPage(x, y) {
+		
+		// Ignore clicks while asleep
+		if (this.asleep) {
+			return;
+		}
+		
+		// Move the cat
 		this.targetX = x - 25;
 		this.targetY = y - 25;
 		
 		if (!this.moving) {
+			// Set the cat moving if it's not already
+			this.preventSleep();
 			const boundMoveImpl = this.moveImpl.bind(this);
 			this.then = Date.now();
 			this.moving = true;
@@ -69,6 +115,7 @@ class Cat {
 		} else {
 			this.elem.style.transform = "";
 		}
+		
 		// Step up the animation index
 		this.moveAnimationIndex = (this.moveAnimationIndex + 1) % WALK_ANIMATION_FRAMES.length;
 		this.elem.style.backgroundPosition = WALK_ANIMATION_FRAMES[this.moveAnimationIndex];
@@ -79,6 +126,9 @@ class Cat {
 			// Stop calling the move function
 			self.moving = false;
 			clearInterval(self.moveInterval);
+			
+			// Send the cat to sleep soon
+			this.queueSleep();
 			
 		} else {
 			// Move the cat an increment
@@ -93,5 +143,8 @@ const cat = new Cat(document.getElementById("cat"));
 document.body.addEventListener('click', (event) => {
 	const x = event.pageX;
 	const y = event.pageY;
-	cat.move(x, y);
+	cat.clickPage(x, y);
+});
+cat.elem.addEventListener('click', (event) => {
+	cat.clickCat();
 });
